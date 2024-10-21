528d728b532d314c10f250469841b8b0a9e3dc7e
Gustavo Henrique Santos Souza de Miranda <gustavohssmiranda@gmail.com>
2024-10-21 20:12:16 -0300

    Created the Migration Function that will generate the sqlite file and the tables of the sqlite database.

--- migrate.php
+++ migrate.php
@@ -2,46 +2,6 @@
 
 $dbFile = __DIR__ . '/data.db';
 
-$createEstadoTable = "
-CREATE TABLE IF NOT EXISTS Estado (
-        ID INTEGER PRIMARY KEY,
-        Estado TEXT NOT NULL
-    );
-";
-
-$createCategoriaTable = "
-CREATE TABLE IF NOT EXISTS  Categoria (
-    ID INTEGER PRIMARY KEY,
-    Categoria TEXT NOT NULL
-);
-";
-
-$createRaridadeTable = "
-CREATE TABLE IF NOT EXISTS Raridade (
-    ID INTEGER PRIMARY KEY,
-    Raridade TEXT NOT NULL,
-    fk_Categoria_ID INTEGER,
-    FOREIGN KEY (fk_Categoria_ID) REFERENCES Categoria (ID) ON DELETE SET NULL
-);
-";
-
-$createCartaTable = "
-CREATE TABLE IF NOT EXISTS  Carta (
-    ID INTEGER PRIMARY KEY,
-    Nome TEXT NOT NULL,
-    Valor_Compra INTEGER,
-    Valor_Mercado INTEGER,
-    Valor_Venda INTEGER,
-    E_Foil INTEGER,
-    E_De_Interesse INTEGER,
-    fk_Categoria_ID INTEGER,
-    fk_Estado_ID INTEGER,
-    fk_Raridade_ID INTEGER,
-    FOREIGN KEY (fk_Categoria_ID) REFERENCES Categoria (ID) ON DELETE CASCADE,
-    FOREIGN KEY (fk_Estado_ID) REFERENCES Estado (ID) ON DELETE CASCADE,
-    FOREIGN KEY (fk_Raridade_ID) REFERENCES Raridade (ID) ON DELETE CASCADE
-);
-";
 $insertCategories = "
 INSERT INTO Categoria (Categoria) VALUES ('Pokemon'),('Magic: The Gathering'),('Yu-Gi-Oh')";
 
@@ -78,31 +38,62 @@ INSERT INTO Estado (Estado) VALUES
 ;
 ";
 
-try {
-    $db = new PDO('sqlite:' . $dbFile);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->exec("PRAGMA foreign_keys = ON;");
-    $db->exec($createEstadoTable);
-    $db->exec($createCategoriaTable);
-    $db->exec($createRaridadeTable);
-    $db->exec($createCartaTable);
-    $db->exec($insertCategories);
-    $db->exec($insertRaridades);
-    $db->exec($insertEstados);
-
-
-}catch (PDOException $e){
-    echo "Error on Migrating: " . $e->getMessage();
-    exit;
-}
-$db = null;
-
 function Migrate_App($db_file)
 {
+    $createEstadoTable = "
+CREATE TABLE IF NOT EXISTS Estado (
+        ID INTEGER PRIMARY KEY,
+        Estado TEXT NOT NULL
+    );
+";
+    $createCategoriaTable = "
+CREATE TABLE IF NOT EXISTS  Categoria (
+    ID INTEGER PRIMARY KEY,
+    Categoria TEXT NOT NULL
+);
+";
+
+    $createRaridadeTable = "
+CREATE TABLE IF NOT EXISTS Raridade (
+    ID INTEGER PRIMARY KEY,
+    Raridade TEXT NOT NULL,
+    fk_Categoria_ID INTEGER,
+    FOREIGN KEY (fk_Categoria_ID) REFERENCES Categoria (ID) ON DELETE SET NULL
+);
+";
+
+    $createCartaTable = "
+CREATE TABLE IF NOT EXISTS  Carta (
+    ID INTEGER PRIMARY KEY,
+    Nome TEXT NOT NULL,
+    Valor_Compra INTEGER,
+    Valor_Mercado INTEGER,
+    Valor_Venda INTEGER,
+    E_Foil INTEGER,
+    E_De_Interesse INTEGER,
+    fk_Categoria_ID INTEGER,
+    fk_Estado_ID INTEGER,
+    fk_Raridade_ID INTEGER,
+    FOREIGN KEY (fk_Categoria_ID) REFERENCES Categoria (ID) ON DELETE CASCADE,
+    FOREIGN KEY (fk_Estado_ID) REFERENCES Estado (ID) ON DELETE CASCADE,
+    FOREIGN KEY (fk_Raridade_ID) REFERENCES Raridade (ID) ON DELETE CASCADE
+);
+";
+
     try {
+        $database = new PDO('sqlite:' . $db_file);
+        $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $database->exec("PRAGMA foreign_keys = ON;");
+        $database->exec($createEstadoTable);
+        $database->exec($createCategoriaTable);
+        $database->exec($createRaridadeTable);
+        $database->exec($createCartaTable);
+
+
 
     }catch (PDOException $e){
-        echo "Error on Migrating: " . $e->getMessage();
+        echo "Error on Migration: " . $e->getMessage();
     }
-    $db = null;
+    $database = null;
 }
+Migrate_App($dbFile);
